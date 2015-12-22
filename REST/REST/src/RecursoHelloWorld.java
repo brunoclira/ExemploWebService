8852288a96250106cfe962966688c4338836a339
Bruno Lira <brunolyr4@gmail.com>
2015-12-22 00:52:44 -0200

    update

---
+++ REST/REST/src/RecursoHelloWorld.java
@@ -0,0 +1,14 @@
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+import javax.ws.rs.Produces; 
+
+@Path("/helloworld")
+
+public class RecursoHelloWorld {
+	@GET
+	@Produces("text/plain")
+	public String exibir(){
+		return "Hello World";
+	}
+}
+
